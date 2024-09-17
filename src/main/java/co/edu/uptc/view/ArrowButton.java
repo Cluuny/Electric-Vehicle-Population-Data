adbdeb5654b82131ae71057dd19214d3222c8ede
Vicente Matallana <vicente.matallana@uptc.edu.co>
2024-09-17 09:26:17 -0500

    Correciones de estilo.

--- src/main/java/co/edu/uptc/view/ArrowButton.java
+++ src/main/java/co/edu/uptc/view/ArrowButton.java
@@ -13,19 +13,14 @@ public class ArrowButton extends JButton {
         super("");
         // Establece un tamaño preferido para el botón
         setPreferredSize(new Dimension(40, 40));
-        
-        ImageIcon originalIcon = new ImageIcon("Electric-Vehicle-Population-Data\\src\\main\\java\\co\\edu\\uptc\\view\\arrow.png");
+
+        ImageIcon originalIcon = new ImageIcon(
+                "Electric-Vehicle-Population-Data\\src\\main\\java\\co\\edu\\uptc\\view\\arrow.png");
         Image imagenEscalada = originalIcon.getImage().getScaledInstance(25, 25, Image.SCALE_SMOOTH);
         ImageIcon scaleIcon = new ImageIcon(imagenEscalada);
 
-        
         this.setIcon(scaleIcon);
-        
-        
-        
+
     }
 
-    
-
-    
-}    
+}
